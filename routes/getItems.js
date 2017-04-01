9f172088fdbb194a771ab245102996e49c9a9d60
David Van <David@DavidVan.net>
2017-04-01 06:39:53 -0700

    Made getItems work with firebase database.

--- routes/getItems.js
+++ routes/getItems.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var database = require('../firebase');
+
 router.post('/', function(req, res) {
-    var items = {
-        1 : ['food', 13.25],
-        2 : ['food2', 13.22]
-    };
-    res.json(JSON.stringify(items));
+    database.getData(function(products) {
+        res.json(products);
+    });
 });
 
 module.exports = router;
