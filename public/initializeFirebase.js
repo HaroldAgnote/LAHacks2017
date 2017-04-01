9cd66b5d5338904f7fe92bc5f136818709f69e1f
Harold Agnote <haroldagnote38@hotmail.com>
2017-04-01 06:17:11 -0700

    Modified database initialization and added item pictures

--- public/initializeFirebase.js
+++ public/initializeFirebase.js
@@ -3,46 +3,41 @@
  */
 var firebase = require('../firebase');
 
-function writeProducts(itemID, name, color, price) {
+function writeProducts(itemID, name, color, price, imageURL) {
     firebase.database().ref('products/' + itemID).set({
         itemName: name,
         itemColor: color,
-        itemPrice: price
+        itemPrice: price,
+        itemImageURL: imageURL
     });
 }
 
 console.log("Adding Item");
 
-writeProducts(3213, "TestName", "Blue", 2.99);
+writeProducts(1232, "Awesome Hat", "White", 3.99, "/images/awesomeHat.jpg");
 
 console.log("Item Added");
 
 console.log("Adding Item");
 
-writeProducts(1232, "Awesome Hat", "White", 3.99);
+writeProducts(2313, "Awesome Shirt", "Blue", 4.99, "/images/shirt.jpg");
 
 console.log("Item Added");
 
 console.log("Adding Item");
 
-writeProducts(2313, "Awesome Shirt", "Blue", 4.99);
+writeProducts(8523, "Awesome Pants", "Black", 9.99, "/images/pants.jpg");
 
 console.log("Item Added");
 
 console.log("Adding Item");
 
-writeProducts(8523, "Awesome Pants", "Black", 9.99);
+writeProducts(1423, "Awesome Jacket", "Red", 12.99, "/images/awesomeJacket.jpg");
 
 console.log("Item Added");
 
 console.log("Adding Item");
 
-writeProducts(1423, "Awesome Jacket", "Red", 12.99);
-
-console.log("Item Added");
-
-console.log("Adding Item");
-
-writeProducts(4134, "Awesome Shoes", "Black", 13.99);
+writeProducts(4134, "Awesome Shoes", "Black", 13.99, "/images/shoes.jpg");
 
 console.log("Item Added");
