9f55f3b8a7b2bed6b8ab9d33ed82fdb20419e264
David Van <David@DavidVan.net>
2017-04-01 00:54:18 -0700

    Updated styling and removed node_modules again.

--- routes/getItemPrice.js
+++ routes/getItemPrice.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
-    res.get("Something");
-    var itemID= req.body.ID;
+router.get('/', function(req, res) {
+    var itemID = req.body.itemID;
+    console.log(req.body);
     var items = {
-        1 : ["food", 13.25],
-        2 : ["food2", 13.22]
+        1 : ['food', 13.25],
+        2 : ['food2', 13.22]
     };
     res.send(items[itemID][1]); // items.get(itemID)[1]
-    res.end();
 });
 
 module.exports = router;
