5ff5ff624dc24836734c8eeda06b9a53545107c0
Ryan Ea <helloxhi1@gmail.com>
2017-04-01 05:05:41 -0700

    ?????

--- public/cart.js
+++ public/cart.js
@@ -33,11 +33,26 @@ function getCart()
     return cart;
 }
 
-function getItemPrice(itemID) {
-    // XmlHttpRequest
-    // GET endpoint /getItemPrice
-    // return result
+function getItemPrice(itemID, callBackFunc) {
+    itemID=1;
+    var request = new XMLHttpRequest();
+    request.open('POST' , '/getItemPrice', true);
+    
+    request.onload = function(){
+        if(request.status >= 200 && request.status<400){
+            callBackFunc(request.responseText);
+        }
+        else{
+            console.log("Failed to load");
+        }
+    };
+    request.onerror = function(){
+        console.log("Connection Fail");
+    }
+    request.send(itemID);
+    
 }
+
 
 function getItems(callback) {
     var request = new XMLHttpRequest();
