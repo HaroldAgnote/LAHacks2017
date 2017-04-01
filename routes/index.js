6c305671123e97fcbd037ffa2e905c18b57a1337
Harold Agnote <haroldagnote38@hotmail.com>
2017-04-01 05:25:25 -0700

    Worked on index files

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,19 @@ router.get('/', function(req, res) {
     var global_products = ''
 
     getData(function(products){
-        global_products = products
+        global_products = products;
+        console.log("In JS")
+        console.log(global_products)
+        console.log(Object.keys(global_products))
+
+        var data = [1,2,3,4,5];
+
+        res.render('index', {
+            products : data
+
+        });
     })
 
-    console.log(global_products)
-    res.render('index', {
-        products : global_products
-    });
 });
 
 module.exports = router;
