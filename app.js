05f9fe04068d4009b90168de4ace2aabd35e3809
Mark Spencer Tan <msgtan@gmail.com>
2017-04-01 00:37:58 -0700

    added back route to index

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var bodyparser = require('body-parser');
 
 var getItemPrice = require('./routes/getItemPrice');
 
+
 app.set('view engine', 'ejs')
 app.use(bodyparser.json());
 app.use('/getItemPrice', getItemPrice);
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
