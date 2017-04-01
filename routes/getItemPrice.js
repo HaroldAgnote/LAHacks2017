1a387c0f03614ca5860552465ad01b05696cc13c
Ryan Ea <helloxhi1@gmail.com>
2017-04-01 08:10:51 -0700

    added code

--- routes/getItemPrice.js
+++ routes/getItemPrice.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var database = require('../firebase');
 router.get('/', function(req, res) {
     res.send('Nothing to see here');
 });
 
 router.post('/', function(req, res) {
-    var itemID = req.body.itemID;
-    console.log(req.body);
-    var items = {
-        1 : ['food', 13.25],
-        2 : ['food2', 13.22]
-    };
-    res.write(JSON.stringify(items[itemID][1]));
-    res.end();
+  	// varitemid
+  var itemID= req.body.itemID;
+  database.getData(function(products) {
+      // do something with products
+//        console.log(products[itemID].itemPrice);
+      res.send((products[itemID].itemPrice).toString());
+//        res.send(JSON.stringify(products[itemID].itemPrice.toString));
+    });
+//    var itemID = req.body.itemID;
+//    console.log(req.body);
+//    var items = {
+//        1 : ['food', 13.25],
+//        2 : ['food2', 13.22]
+//    };
+//    res.write(JSON.stringify(items[itemID][1]));
+//    res.end();
     //res.sendStatus(items[itemID][1]); // items.get(itemID)[1]
 });
 
