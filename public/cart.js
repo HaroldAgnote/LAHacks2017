5c41cf5b9b2e395f8ba5cd7e3d25f931d0606392
Harold Agnote <haroldagnote38@hotmail.com>
2017-04-01 05:57:34 -0700

    Implemented item information

--- public/cart.js
+++ public/cart.js
@@ -4,19 +4,6 @@ var database = firebase.database();
 
 var cart = [];
 
-var getData = function(callback){ database.ref("/products/").once('value').then(function(snapshot, product){
-    product = snapshot.val();
-    console.log(product)
-    callback(product)
-})};
-
-var global_products = ''
-getData(function(products){
-	global_products = products
-})
-
-console.log(global_products)
-
 
 function addToCart(product)
 {
