e937991eeb721cda487f1efc6cd0ee9de991ee62
David Van <David@DavidVan.net>
2017-04-01 10:46:23 -0700

    Re-organized stuff.

--- routes/getItemPrice.js
+++ routes/getItemPrice.js
@@ -6,23 +6,10 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  	// varitemid
-  var itemID= req.body.itemID;
-  database.getData(function(products) {
-      // do something with products
-//        console.log(products[itemID].itemPrice);
-      res.send((products[itemID].itemPrice).toString());
-//        res.send(JSON.stringify(products[itemID].itemPrice.toString));
+    var itemID= req.body.itemID;
+    database.getData(function(products) {
+        res.send((products[itemID].itemPrice).toString());
     });
-//    var itemID = req.body.itemID;
-//    console.log(req.body);
-//    var items = {
-//        1 : ['food', 13.25],
-//        2 : ['food2', 13.22]
-//    };
-//    res.write(JSON.stringify(items[itemID][1]));
-//    res.end();
-    //res.sendStatus(items[itemID][1]); // items.get(itemID)[1]
 });
 
 module.exports = router;
