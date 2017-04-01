8d232bc5eeb85b1aed1b232435dc4845b71b57c3
Kenneth Ung <kennethleeung@gmail.com>
2017-04-01 00:49:20 -0700

    Fixed errors in app.js

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
 
-var app = require('express');
-var ejs = require('ejs')
+var index = require('./routes/processtransaction');
+app.use('/',index);
+
 var bodyparser = require('body-parser');
 
 var getItemPrice = require('./routes/getItemPrice');
 
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(bodyparser.json());
 app.use('/getItemPrice', getItemPrice);
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index');
+
 })
 
 app.listen(3000, function () {
