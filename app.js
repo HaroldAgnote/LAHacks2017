d195a507c15f7be14f8f3d3b9f50139a6b1ca8bd
David Van <David@DavidVan.net>
2017-04-01 03:30:56 -0700

    Modified app.js file.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+var bodyParser = require('body-parser');
 var express = require('express');
-var app = express();
 var ejs = require('ejs');
 var path = require('path')
+
+var app = express();
 
 var firebase = require('./firebase');
 
@@ -9,23 +11,16 @@ var cart = require('./public/cart')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var index = require('./routes/processtransaction');
-app.use('/process',index);
-
-var bodyParser = require('body-parser');
-
+var index = require('./routes/index');
 var getItemPrice = require('./routes/getItemPrice');
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', index);
 app.use('/getItemPrice', getItemPrice);
-
-app.get('/home', function (req, res) {
-  res.render('index');
-})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
